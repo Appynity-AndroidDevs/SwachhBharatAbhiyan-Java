c16000c192047a1a602cc286f2c1dcac30b8438b
Rahul9545 <99891574+Rahul9545@users.noreply.github.com>
2022-12-23 11:29:01 +0530

    committing Changes : survey font family added

---
+++ swachbharatabhiyanlibrary/src/main/java/com/appynitty/swachbharatabhiyanlibrary/adapters/UI/SurveyPagerAdapter.java
@@ -0,0 +1,21 @@
+package com.appynitty.swachbharatabhiyanlibrary.adapters.UI;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class SurveyPagerAdapter extends PagerAdapter {
+    private Context context;
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return false;
+    }
+}
